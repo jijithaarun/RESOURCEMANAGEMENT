cdaec82ae2e8aa05b775269a75c39b926e7d073a
asha <ashakrishnan1997@gmail.com>
2020-03-24 14:35:09 +0530

    asha fifth commit

--- ResourceManagementREST/src/main/java/com/resource/app/controller/PaymentController.java
+++ ResourceManagementREST/src/main/java/com/resource/app/controller/PaymentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.resource.app.model.Payment;
@@ -23,7 +24,7 @@ public class PaymentController {
 
 	// adding the payment details
 	@PostMapping("payment")
-	public ResponseEntity<Payment> addPayment(Payment payment) {
+	public ResponseEntity<Payment> addPayment(@RequestBody Payment payment) {
 		return new ResponseEntity<Payment>(paymentService.addPayment(payment), HttpStatus.OK);
 
 	}
