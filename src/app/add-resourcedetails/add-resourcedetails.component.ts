814a9e383d5cf7f1ba26fd613b3832c64f6b0234
jijithaarun <jijitha.mj1990@gmail.com>
2020-04-13 15:38:26 +0530

    my second commit

--- src/app/add-resourcedetails/add-resourcedetails.component.ts
+++ src/app/add-resourcedetails/add-resourcedetails.component.ts
@@ -7,6 +7,7 @@ import { ResourcedetailsService } from '../resourcedetails.service';
 import { Resource } from '../resource';
 import { ResourceService } from '../resource.service';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-resourcedetails',
@@ -19,18 +20,22 @@ export class AddResourcedetailsComponent implements OnInit {
   res: Resourcedetails = new Resourcedetails();
   resourceType: Observable<Resource[]>;
   addResourceType: FormGroup;
-  public resourceFile: any = File;
+   resourceFile: File = null;
+   imageURL="assets/image/"
+  
 
   constructor(private formBuilder: FormBuilder,
     private resourceDetailsService: ResourcedetailsService,
     private resourceService: ResourceService,
     private router: Router,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private http:HttpClient) { }
 
   ngOnInit(): void {
     this.createForm();
     this.getResources();
   }
+
 
   createForm() {
 
@@ -59,6 +64,23 @@ export class AddResourcedetailsComponent implements OnInit {
 
   }
 
+  onUpload()
+  {
+    const formData=new FormData();
+    formData.append('photo',this.resourceFile,this.resourceFile.name);
+  
+    this.http.post('assets/image/',formData,{observe:'response'}).subscribe((response)=>{
+      if(response.status===200)
+      {
+        this.toastr.success('Image successfully added');
+      }else
+      {
+        this.toastr.error('Image not successfully added');
+      }
+    });
+  }
+
+
   onSubmit() {
     // assigning values into resource from fromGroup
     // this.res = this.addResourceForm.value;
@@ -66,13 +88,13 @@ export class AddResourcedetailsComponent implements OnInit {
     // console.log(this.addResourceForm.controls.resourceTypeId.value);
 
     //setting values
-    /*this.res.resource = this.addResourceType.value;
+    this.res.resource = this.addResourceType.value;
     this.res.resourceCapacity = this.addResourceForm.controls.resourceCapacity.value;
     this.res.noOfSystems = this.addResourceForm.controls.noOfSystems.value;
     this.res.projector = this.addResourceForm.controls.projector.value;
     this.res.whiteBoard = this.addResourceForm.controls.whiteBoard.value;
     this.res.resourceRate = this.addResourceForm.controls.resourceRate.value;
-    this.res.photo = this.resourceFile;
+    
 
     this.res.typeOfUse = "";
     this.res.isActive = "Y";
@@ -81,46 +103,20 @@ export class AddResourcedetailsComponent implements OnInit {
     console.log(this.res);
 
     // calling method to insert
-    this.resourceDetailsService.createResource(this.res).subscribe(
+    const formDatas=new FormData();
+     formDatas.append('resource', JSON.stringify(this.res));
+     
+    formDatas.append('file', this.resourceFile,this.resourceFile.name);
+
+  
+    this.resourceDetailsService.createResource(formDatas).subscribe(
       data => console.log(data), error => console.log(error)
     );
     this.toastr.success('New Resource Successfully Created', 'Creating ResourceType');
-    this.router.navigateByUrl('/admin/viewResourceDetails');*/
+    this.router.navigateByUrl('/admin/viewResourceDetails');
 
-    if (this.addResourceForm.valid) {
-      this.res.resource = this.addResourceType.value;
-      this.res.resourceCapacity = this.addResourceForm.controls.resourceCapacity.value;
-      this.res.noOfSystems = this.addResourceForm.controls.noOfSystems.value;
-      this.res.projector = this.addResourceForm.controls.projector.value;
-      this.res.whiteBoard = this.addResourceForm.controls.whiteBoard.value;
-      this.res.resourceRate = this.addResourceForm.controls.resourceRate.value;
-
-      this.res.typeOfUse = "";
-      this.res.isActive = "Y";
-      this.res.isAccepted = "N";
-      this.res.isBooked = "N";
-      console.log(this.res);
-
-      const formData = new FormData();
-      formData.append('resource', JSON.stringify(this.res));
-     
-      formData.append('file', this.resourceFile,this.resourceFile.name);
-      
-
-      this.resourceDetailsService.createResource(formData).subscribe((Response) => {
-        console.log(Response);
-        if (Response.status == 'error') {
-          this.toastr.error('data not created', 'creating resource type');
-          this.router.navigateByUrl('/coordinator/viewResourceDetails');
-        }
-        else {
-          this.toastr.success('New Resource Successfully Created', 'Creating ResourceType');
-          this.router.navigateByUrl('/coordinator/viewResourceDetails');
-        }
-
-      },
-      error=>console.log(error));
-    }
+    
+    
   } 
 
   getResources() {
--- src/app/registration.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Registration } from './registration';
-
-describe('Registration', () => {
-  it('should create an instance', () => {
-    expect(new Registration()).toBeTruthy();
-  });
-});
